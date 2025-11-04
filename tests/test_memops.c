3ba7231698ccd953f453571aab0763851f6831bc
manyids2 <arun.mukundan@gmail.com>
2025-11-04 19:08:45 +0100

    YES: Clamping down memops for now with asserts

--- tests/test_memops.c
+++ tests/test_memops.c
@@ -16,21 +16,21 @@ int main()
 
     char  dst_16_i[frames * skip_16];
     float dst_16_f[frames];
-    sample_move_d16_sS(dst_16_i, range, skip_16, 1, frames);
-    sample_move_dS_s16(dst_16_f, dst_16_i, 1, skip_16, frames);
+    sample_move_d16_sS(dst_16_i, range, skip_16, skip_32, frames);
+    sample_move_dS_s16(dst_16_f, dst_16_i, skip_32, skip_16, frames);
 
     print_range(dst_16_f, frames, "%+2.2f | ");
 
     char  dst_24_i[frames * skip_24];
     float dst_24_f[frames];
-    sample_move_d24_sS(dst_24_i, range, skip_24, 1, frames);
-    sample_move_dS_s24(dst_24_f, dst_24_i, 1, skip_24, frames);
+    sample_move_d24_sS(dst_24_i, range, skip_24, skip_32, frames);
+    sample_move_dS_s24(dst_24_f, dst_24_i, skip_32, skip_24, frames);
     print_range(dst_24_f, frames, "%+2.2f | ");
 
     char  dst_32_i[frames * skip_32];
     float dst_32_f[frames];
-    sample_move_d32_sS(dst_32_i, range, skip_32, 1, frames);
-    sample_move_dS_s32(dst_32_f, dst_32_i, 1, skip_32, frames);
+    sample_move_d32_sS(dst_32_i, range, skip_32, skip_32, frames);
+    sample_move_dS_s32(dst_32_f, dst_32_i, skip_32, skip_32, frames);
     print_range(dst_32_f, frames, "%+2.2f | ");
 
     return EXIT_SUCCESS;
